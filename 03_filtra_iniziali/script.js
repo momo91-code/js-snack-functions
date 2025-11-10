a2b8250e30cf004f4712770e4af8bffad3b0d0bb
monica <monica.poli91@libero.it>
2025-11-10 17:55:19 +0100

    filtra iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,15 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterByLetter(array, letter){
+    return array.filter(function(name) {
+        return name[0] === letter;
+    });
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const filteredNames = filterByLetter(names, "A");
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
