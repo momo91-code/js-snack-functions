bdffd7b2f0cdd4a728f678a383f1b692f671d91a
monica <monica.poli91@libero.it>
2025-11-10 18:13:13 +0100

    conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(str) {
+    let conta = 0;
+    const vocali = 'aeiou';
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+    for (let i = 0; i < str.length; i ++){
+        let lettera = str[i].toLowerCase();
+        if (vocali.includes(lettera)) {
+            conta++;
+        }
+    }
+    return conta;
+}
+console.log(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
