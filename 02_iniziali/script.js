6f2a67d3cb6325586eb4879ff61d8928e8b8e077
monica <monica.poli91@libero.it>
2025-11-10 17:45:08 +0100

    iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getInitials(array) {
+    const initials = [];
+    for (let i = 0; i < array.length; i ++){
+        const name = array[i];
+        const initial = name[0];
+        initials.push(initial);
+    }
+    return initials;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = getInitials(names);
+console.log(result);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
